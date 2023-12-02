6b5ecc11d6eefd6c20f5d3291ff27a5aea24e715
JOSUEPORTALES <143145186+JOSUEPORTALES@users.noreply.github.com>
2023-12-01 23:11:05 -0700

    Agregando Archivos png

--- src/GameOver.cpp
+++ src/GameOver.cpp
@@ -7,35 +7,137 @@
 //Descripcion: Implementaicion de la clase GameOver
 //=======================================================================
 
-#pragma once
 
-#include <memory>
 
-#include <SFML/Graphics/Text.hpp>
+#include "GameOver.hpp"
+#include "GamePlay.hpp"
 
-#include "State.hpp"
-#include "Game.hpp"
+#include <SFML/Window/Event.hpp>
 
-class GameOver : public Engine::State
+GameOver::GameOver(std::shared_ptr<Context> &context)
+    : m_context(context), m_isRetryButtonSelected(true),
+      m_isRetryButtonPressed(false), m_isExitButtonSelected(false),
+      m_isExitButtonPressed(false)
 {
-public:
-    GameOver(std::shared_ptr<Context> &context);
-    ~GameOver();
+}
 
-    void Init() override;
-    void ProcessInput() override;
-    void Update(const sf::Time& deltaTime) override;
-    void Draw() override;
+GameOver::~GameOver()
+{
+}
 
-private:
-    std::shared_ptr<Context> m_context;
-    sf::Text m_gameOverTitle;
-    sf::Text m_retryButton;
-    sf::Text m_exitButton;
+void GameOver::Init()
+{
+    // Title
+    m_gameOverTitle.setFont(m_context->m_assets->GetFont(MAIN_FONT));
+    m_gameOverTitle.setString("Game Over");
+    m_gameOverTitle.setOrigin(m_gameOverTitle.getLocalBounds().width / 2,
+                              m_gameOverTitle.getLocalBounds().height / 2);
+    m_gameOverTitle.setPosition(m_context->m_window->getSize().x / 2,
+                                m_context->m_window->getSize().y / 2 - 150.f);
 
-    bool m_isRetryButtonSelected;
-    bool m_isRetryButtonPressed;
+    // Play Button
+    m_retryButton.setFont(m_context->m_assets->GetFont(MAIN_FONT));
+    m_retryButton.setString("Retry");
+    m_retryButton.setOrigin(m_retryButton.getLocalBounds().width / 2,
+                            m_retryButton.getLocalBounds().height / 2);
+    m_retryButton.setPosition(m_context->m_window->getSize().x / 2,
+                              m_context->m_window->getSize().y / 2 - 25.f);
+    m_retryButton.setCharacterSize(20);
 
-    bool m_isExitButtonSelected;
-    bool m_isExitButtonPressed;
-};
+    // Exit Button
+    m_exitButton.setFont(m_context->m_assets->GetFont(MAIN_FONT));
+    m_exitButton.setString("Exit");
+    m_exitButton.setOrigin(m_exitButton.getLocalBounds().width / 2,
+                           m_exitButton.getLocalBounds().height / 2);
+    m_exitButton.setPosition(m_context->m_window->getSize().x / 2,
+                             m_context->m_window->getSize().y / 2 + 25.f);
+    m_exitButton.setCharacterSize(20);
+}
+
+void GameOver::ProcessInput()
+{
+    sf::Event event;
+    while (m_context->m_window->pollEvent(event))
+    {
+        if (event.type == sf::Event::Closed)
+        {
+            m_context->m_states->PopAll();
+        }
+        else if (event.type == sf::Event::KeyPressed)
+        {
+            switch (event.key.code)
+            {
+            case sf::Keyboard::Up:
+            {
+                if (!m_isRetryButtonSelected)
+                {
+                    m_isRetryButtonSelected = true;
+                    m_isExitButtonSelected = false;
+                }
+                break;
+            }
+            case sf::Keyboard::Down:
+            {
+                if (!m_isExitButtonSelected)
+                {
+                    m_isRetryButtonSelected = false;
+                    m_isExitButtonSelected = true;
+                }
+                break;
+            }
+            case sf::Keyboard::Return:
+            {
+                m_isRetryButtonPressed = false;
+                m_isExitButtonPressed = false;
+
+                if (m_isRetryButtonSelected)
+                {
+                    m_isRetryButtonPressed = true;
+                }
+                else
+                {
+                    m_isExitButtonPressed = true;
+                }
+
+                break;
+            }
+            default:
+            {
+                break;
+            }
+            }
+        }
+    }
+}
+
+void GameOver::Update(const sf::Time &deltaTime)
+{
+    if (m_isRetryButtonSelected)
+    {
+        m_retryButton.setFillColor(sf::Color::Black);
+        m_exitButton.setFillColor(sf::Color::White);
+    }
+    else
+    {
+        m_exitButton.setFillColor(sf::Color::Black);
+        m_retryButton.setFillColor(sf::Color::White);
+    }
+
+    if (m_isRetryButtonPressed)
+    {
+        m_context->m_states->Add(std::make_unique<GamePlay>(m_context), true);
+    }
+    else if (m_isExitButtonPressed)
+    {
+        m_context->m_states->PopAll();
+    }
+}
+
+void GameOver::Draw()
+{
+    m_context->m_window->clear(sf::Color::Blue);
+    m_context->m_window->draw(m_gameOverTitle);
+    m_context->m_window->draw(m_retryButton);
+    m_context->m_window->draw(m_exitButton);
+    m_context->m_window->display();
+}
